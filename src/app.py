9a045431957590ca21e8a95e7976d7febe34c06b
IcQuackson <pedrorenato1998@gmail.com>
2024-04-03 11:44:40 +0100

    Implemented text and file upload. - Organized directories

--- src/app.py
+++ src/app.py
@@ -1,16 +1,11 @@
 from flask import Flask, request, jsonify, render_template
 from flask import send_from_directory
-#from openai_functions import call_openai
-import re
-import requests
-import sys
 import os
 from openai import AzureOpenAI
 from dotenv import load_dotenv
 load_dotenv()
 
-
-
+# Load the Azure OpenAI API key from the environment variables
 client = AzureOpenAI(
   azure_endpoint = "https://escola42.openai.azure.com/", 
   api_key=os.getenv("AZURE_OPENAI_API_KEY"),  
@@ -21,59 +16,86 @@ client = AzureOpenAI(
 
 CHAT_COMPLETIONS_MODEL = os.getenv('CHAT_COMPLETION_NAME')
 SEED=123
-
-print(os.getenv('AZURE_OPENAI_API_KEY'))
-
-
+OUTPUT_FOLDER = './out'
 
 app = Flask(__name__)
 
-#curl -X POST -H "Content-Type: application/json" -d "{"user_story": "USER STORY A\n\nPre-condition(s)\n\nThe user has accessed an FO form.\n\nActor(s)\n\nUser.\n\nPost-condition(s)\n\n· The system records the explicit consent of the user together with the date and time stamp of the user’s consent\n\n· The user can start or resume the current application form\n\nRequirements\n\nREQ-UCC06-010 Privacy policy consent\n\nOn applications start or resume, the system displays an overlay or a modal window that lists the\n\ncontent of the Privacy Policy, with options to review and accept.\n\nIf the user accepts, the user can proceed with the current application.\n\nIf the user does not accept, the user cannot proceed with the current application and remains on\n\nthe initial step of the application.\n\nREQ-UCC06-020 Cookies policy\n\nOn applications start or resume, the system displays a header/footer overlay with the content of\n\nthe Cookies policy, with options to review and accept.\n\nThe user only has the option to allow system cookies. There's no option to block the system\n\ncookies.\n\nIf the user does not allow, the user cannot proceed with the current application and remains on\n\nthe initial step of the application.\n\nInformation model\n\n# Field label Description Type M/O/System Rules Visibility\n\n1 Privacy Policy message Content of the Privacy Policy text. Text System The content of the Privacy Policy modal is specific for each IPO. On applications start or resume.\n\n2 Accept and Close Button on privacy policy pop up to 'Accept and close' the pop up. Option M The user can only proceed with the current application in case the Privacy policy has been accepted. On applications start or resume.\n\n3 Cookie Policy Cookies header or footer that overlays the website in order to grant permission. Text O Displayed cookie policy UCC06_MSG_01. The cookie policy link brings you to the relevant cookie policy in a new modal window. On applications start or resume.\n\n4 Cookies acceptance Option to black or allow the cookies. Options M Possible options: · Allow cookies: Clicking on this option allows the website to save cookies on the computer. The user can only proceed with the current application when On applications start.\n\n# Field label Description Type M/O/System Rules Visibility the cookies are allowed.\n\nMessages\n\nCode Type Message\n\nUCC06_MSG_01 Informative This website uses cookies to remember your settings and gather web statistics. You will have to grant permission for the installation of these cookies. More information on our cookie policy.\n\n"}" http://localhost:5000/
-
-
-@app.route('/', methods=['GET'])
+"""
+Endpoint to generate test cases from user story
+Method: POST
+Parameters:	user_story: string or file
+Returns:	test_cases: string
+"""
+@app.route('/', methods=['POST'])
 def generate_test_cases():
 
-  # Read system_message from file
-  with open('system_message.txt', 'r') as file:
-    system_message = file.read()
+	print('ARGUMENTS: ', request.args)
+	print('FILES: ', request.files)
 
-  user_story = "USER STORY A\n\nPre-condition(s)\n\nThe user has accessed an FO form.\n\nActor(s)\n\nUser.\n\nPost-condition(s)\n\n· The system records the explicit consent of the user together with the date and time stamp of the user’s consent\n\n· The user can start or resume the current application form\n\nRequirements\n\nREQ-UCC06-010 Privacy policy consent\n\nOn applications start or resume, the system displays an overlay or a modal window that lists the\n\ncontent of the Privacy Policy, with options to review and accept.\n\nIf the user accepts, the user can proceed with the current application.\n\nIf the user does not accept, the user cannot proceed with the current application and remains on\n\nthe initial step of the application.\n\nREQ-UCC06-020 Cookies policy\n\nOn applications start or resume, the system displays a header/footer overlay with the content of\n\nthe Cookies policy, with options to review and accept.\n\nThe user only has the option to allow system cookies. There's no option to block the system\n\ncookies.\n\nIf the user does not allow, the user cannot proceed with the current application and remains on\n\nthe initial step of the application.\n\nInformation model\n\n# Field label Description Type M/O/System Rules Visibility\n\n1 Privacy Policy message Content of the Privacy Policy text. Text System The content of the Privacy Policy modal is specific for each IPO. On applications start or resume.\n\n2 Accept and Close Button on privacy policy pop up to 'Accept and close' the pop up. Option M The user can only proceed with the current application in case the Privacy policy has been accepted. On applications start or resume.\n\n3 Cookie Policy Cookies header or footer that overlays the website in order to grant permission. Text O Displayed cookie policy UCC06_MSG_01. The cookie policy link brings you to the relevant cookie policy in a new modal window. On applications start or resume.\n\n4 Cookies acceptance Option to black or allow the cookies. Options M Possible options: · Allow cookies: Clicking on this option allows the website to save cookies on the computer. The user can only proceed with the current application when On applications start.\n\n# Field label Description Type M/O/System Rules Visibility the cookies are allowed.\n\nMessages\n\nCode Type Message\n\nUCC06_MSG_01 Informative This website uses cookies to remember your settings and gather web statistics. You will have to grant permission for the installation of these cookies. More information on our cookie policy.\n\n"
+	user_story = ''
 
-  #system_message = "hello"
-  #user_story = "hello"
+	# Check if the request contains a file
+	if 'file' in request.files:
+		file = request.files['file']
+		user_story = file.read().decode('utf-8')
+		print('GOT FILE')
+	else:
+		# Read plain text from request
+		user_story = request.form.get('text')
+		print('GOT TEXT')
 
-  message_text = [{"role":"system","content": system_message}, {"role":"user","content": user_story}]
+	#Remove every &nbsp; from the user story
+	user_story = user_story.replace('&nbsp;', ' ')
 
-  #user_story = request.json.get('user_story')
+	# Read system_message from file
+	with open('system_message.txt', 'r') as file:
+		system_message = file.read()
 
-  response = client.chat.completions.create(
-    model="london_is_best", # model = "deployment_name"
-    messages = message_text,
-    temperature=0.7,
-    max_tokens=4096,
-    top_p=0.95,
-    frequency_penalty=0,
-    presence_penalty=0,
-    stop=None
-  )
+	message_text = [{"role":"system","content": system_message}, {"role":"user","content": user_story}]
 
-  # Extracting generated test cases from the completion
-  print(response)
-  test_cases = response.choices[0].message.content
+	response = client.chat.completions.create(
+	model="london_is_best",
+	messages = message_text,
+	temperature=0.5,
+	max_tokens=4096,
+	top_p=0.95,
+	frequency_penalty=0,
+	presence_penalty=0,
+	stop=None
+	)
 
-  # write test_cases in a file
-  with open('test_cases.txt', 'w') as file:
-    file.write(test_cases)
+	# Extracting generated test cases from the completion
+	test_cases = response.choices[0].message.content
+	data = jsonify({"test_cases": test_cases})
 
-  # Returns the test cases generated by the model
-  return jsonify({"test_cases": test_cases})
+	# write test_cases in a file
+	with open(OUTPUT_FOLDER + '/test_cases.txt', 'w') as file:
+		file.write(test_cases)
 
+	html_response = f"""
+    <html>
+    <head>
+        <title>Test Cases</title>
+    </head>
+    <body>
+        <h1>Generated Test Cases</h1>
+        <h2>User Story:</h2>
+        <pre>{test_cases}</pre>
+    </body>
+    </html>
+    """
+
+	# Returns the test cases generated by the model
+	return html_response
+
+"""
+Endpoint to return index.html
+Parameters:	None
+Returns:	index.html
+"""
 @app.route('/index.html', methods=['GET'])
 def get_index():
-  # print current directory
-  print(os.getcwd())
-  return send_from_directory('static', 'index.html')
+	return send_from_directory('static', 'index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
